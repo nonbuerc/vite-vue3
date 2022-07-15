3509c9cc947988b91b7788058240e3a07396a68a
cheng <gnehcheng@yeah.net>
2022-07-15 14:59:41 +0800

    .

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,13 +1,21 @@
 <script setup>
 import { defStore } from '../store/index'
+import { useRouter } from 'vue-router'
+
+import { Menu } from './index'
 </script>
 
 <template>
-  <q-header v-model="defStore().config.isReveal" reveal elevated>
+  <q-header
+    v-model="defStore().config.isReveal"
+    reveal
+    elevated
+    style="color: inherit; background: inherit"
+  >
     <q-toolbar>
       <div class="q-gutter-md">
         <q-btn
-          v-if="defStore().config.showDrawerMenu || false"
+          v-if="defStore().config.menuPosition === 'left' && defStore().config.showMenu"
           @click="
             defStore().$patch((state) => (state.config.drawerMenu = !state.config.drawerMenu))
           "
@@ -21,8 +29,16 @@ import { defStore } from '../store/index'
           </q-avatar>
         </q-btn>
       </div>
-      <q-toolbar-title> <strong>Header</strong></q-toolbar-title>
 
+      <q-toolbar-title>
+        <strong>Header</strong>
+      </q-toolbar-title>
+      <Menu
+        v-if="defStore().config.menuPosition === 'header' && defStore().config.showMenu"
+        :routes="useRouter().options.routes"
+        :selMenu="defStore().selMenu"
+        mode="horizontal"
+      ></Menu>
       <div class="q-gutter-md">
         <q-btn round>
           <q-avatar>
