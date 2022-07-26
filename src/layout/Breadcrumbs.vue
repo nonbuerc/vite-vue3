99c38e701a19c27d9c5637b033c20e12dfb77c3d
cheng <gnehcheng@yeah.net>
2022-07-26 16:11:25 +0800

    .

--- src/layout/Breadcrumbs.vue
+++ src/layout/Breadcrumbs.vue
@@ -1,12 +1,17 @@
 <script setup>
 import { defStore } from '../store/index'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+console.log(route.matched)
 </script>
 <template>
   <q-breadcrumbs class="inset-shadow q-px-md q-py-xs" v-if="defStore().config.breadcrumbs">
     <!-- shadow-3 -->
-    <q-breadcrumbs-el icon="home" to="/" />
-    <q-breadcrumbs-el label="Docs" icon="widgets" to="/" />
-    <q-breadcrumbs-el label="Breadcrumbs" icon="navigation" to="/" />
-    <q-breadcrumbs-el label="Build" />
+    <q-breadcrumbs-el
+      v-for="(v, i) in route.matched"
+      :label="v.meta.label"
+      :icon="v.meta.icon"
+      :to="{ name: v.name }"
+    />
   </q-breadcrumbs>
 </template>
