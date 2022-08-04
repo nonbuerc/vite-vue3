90d88b2e8591134b30b3496920d66fe52f601908
cheng <gnehcheng@yeah.net>
2022-08-04 11:57:02 +0800

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,7 @@ import { ref, watch } from 'vue'
 import { date } from 'quasar'
 
 const timeStamp = Date.now()
-console.log(timeStamp)
 const formattedString = date.formatDate(timeStamp, 'HH:mm:ss')
-console.log(formattedString)
 const text = ref('123')
 const time = ref(new Date().getTime)
 </script>
